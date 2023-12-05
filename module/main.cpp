06b3ee303f0dba8f05a35692fcb5e98bb6f2b9cd
Riley <rileyk2809@gmail.com>
2023-12-04 19:21:43 -0800

    some stuff, y'know

--- module/main.cpp
+++ module/main.cpp
@@ -20,38 +20,61 @@
 #include <ws2tcpip.h>
 
 
-int main() {
-
+int main(int argc, char* argv[]) {
     WSADATA wsaData;
+
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "wsaStartup failed" << std::endl;
-        exit(1);
+        std::cerr << "WSAStartup failed" << std::endl;
+        return 1;
     }
 
-    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
-        std::cerr << "version 2.2 unavailable" << std::endl;
+    if (argc != 2) {
+        std::cerr << "usage: showip hostname\n";
         WSACleanup();
-        exit(2);
+        return 2;
     }
 
+    struct addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+        std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
+        WSACleanup();
+        return 3;
+    }
+
+    std::cout << "IP addresses for " << argv[1] << ":\n\n";
+
+    for (p = res; p != NULL; p = p->ai_next) {
+        void* addr;
+        const char* ipver;
+
+        // get the pointer to the address itself,
+        // different fields in IPv4 and IPv6:
+        if (p->ai_family == AF_INET) { // IPv4
+            struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        }
+
+        else { // IPv6
+            struct sockaddr_in6* ipv6 = (struct sockaddr_in6*)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        // convert the IP to a string and print it:
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        std::cout << " " << ipver << ": " << ipstr << std::endl;
+    }
+
+    freeaddrinfo(res); // free the linked list
     WSACleanup();
-
-    SockAddrIn sa;
-    SockAddrIn6 sa6;
-
-    inet_pton(AF_INET, "10.12.110.57", &(sa.sinAddr.sAddr));
-    inet_pton(AF_INET6, "2001:db8:63b3:1::3490", &(sa6.sin6Addr.s6Addr));
-
-    char ip4[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(sa.sinAddr), ip4, INET_ADDRSTRLEN);
-
-    char ip6[INET6_ADDRSTRLEN]; // space to hold the IPv6 string
-    inet_ntop(AF_INET6, &(sa6.sin6Addr), ip6, INET6_ADDRSTRLEN);
-
-    std::cout << "The IPv4 address is: " << ip4 << std::endl;
-    std::cout << "The IPv6 address is: " << ip6 << std::endl;
-
 
     return 0;
 }
-
